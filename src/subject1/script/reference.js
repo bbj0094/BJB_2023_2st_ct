a20fc63cb77ed7d0192f429bd4f16b0b2837a867
bbj0094 <139100879+bbj0094@users.noreply.github.com>
2023-10-01 01:15:14 +0900

    인용문추가

--- src/subject1/script/reference.js
+++ src/subject1/script/reference.js
@@ -1,9 +1,10 @@
 const reference = [
   {
-    author: "",
-    title: "",
-    year: "",
-    sourceTitle: "",
-    link: "",
+    author:
+      "Edouard Mathieu, Hannah Ritchie, Lucas Rodés-Guirao, Cameron Appel, Charlie Giattino, Joe Hasell, Bobbie Macdonald, Saloni Dattani, Diana Beltekian, Esteban Ortiz-Ospina and Max Roser",
+    title: "Coronavirus Pandemic (COVID-19)",
+    year: "2022",
+    sourceTitle: "Our World in Data",
+    link: "https://ourworldindata.org/policy-responses-covid#cancellation-of-public-events",
   },
 ];
